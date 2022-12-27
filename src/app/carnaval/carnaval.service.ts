b6562df9ab9714bed21f9b3f591ad6e4445bad42
pegonsLog <pegons.log@gmail.com>
2022-12-27 17:33:35 -0300

    27/12-trabalho

--- src/app/carnaval/carnaval.service.ts
+++ src/app/carnaval/carnaval.service.ts
@@ -1,6 +1,5 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import axios from 'axios';
 import { Observable } from 'rxjs';
 import { Blocos } from './models/bloco';
 
